bafbde31be92a598f04ba6fd6d2d06764066b2de
SaLi <sashalischinskiy72@gmail.com>
2020-12-20 23:53:28 +0200

    0.06

--- greece/src/scripts/index.js
+++ greece/src/scripts/index.js
@@ -3,22 +3,21 @@
 
     inputs.forEach(input => input.setAttribute("autocomplete","off"));
     
-    let array1 = document.getElementsByClassName('places-card__title')
-    let array2 = document.getElementsByClassName('hidden__title')
-    for (let i = 0; i < array2.length; i++) {
-        array2[i].innerHTML = array1[i].innerHTML
-    }
     
-    // Jquery 
+    // import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 
-    //  hiding .places-card__title when hover.
-    // var n;
-    // $( ".places-card" )
-    //     .mouseover(function() {
-    //          n = $( this ).find( ".hidden__title" ).text();
-    //     $( this ).find( ".places-card__title" ).text( "" );
-    //     }).mouseleave(function() {
 
-    //     $( this ).find( ".places-card__title" ).text(n);
-    //     });
-     
+   
+      const swiper = new Swiper('.swiper-container', {
+        cssMode: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+   
