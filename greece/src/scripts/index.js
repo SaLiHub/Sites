b95b557c66b8560f74b8d9c646e9e9fb2353083e
SaLi <sashalischinskiy72@gmail.com>
2020-12-22 23:36:35 +0200

    0.07

--- greece/src/scripts/index.js
+++ greece/src/scripts/index.js
@@ -4,6 +4,32 @@
     inputs.forEach(input => input.setAttribute("autocomplete","off"));
     
     
+
+    
+    
+    // .wrapper__title innerHTML
+    let placesCardsTitles = document.querySelectorAll(".places-card__title"),
+    wraperTitles = document.querySelectorAll(".wrapper__title"),
+    i;
+    for(i = 0; i < placesCardsTitles.length; i++) {
+      wraperTitles[i].innerHTML = placesCardsTitles[i].innerHTML
+    }
+    
+
+
+    // 
+    let placesCards = document.querySelectorAll(".places-card")
+    // let hightOfElement = document.querySelectorAll(".places-card")
+    
+    
+    for (i = 0; i < placesCards.length; i++) {
+       console.log(wraperTitles[i].style.fontSize)
+       wraperTitles[i].style.fontSize  = `${placesCards[i].offsetHeight / 10}px`
+       placesCardsTitles[i].style.fontSize  = `${placesCards[i].offsetHeight / 10}px`
+       console.log(wraperTitles[i].style.fontSize )
+    }
+ 
+
     // import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 
 
