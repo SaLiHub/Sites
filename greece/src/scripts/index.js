95b5d6eeffad3ff39835d761c16c28840b49e314
SaLi <sashalischinskiy72@gmail.com>
2021-01-03 23:57:50 +0200

    0.13

--- greece/src/scripts/index.js
+++ greece/src/scripts/index.js
@@ -2,11 +2,7 @@
     let inputs = document.querySelectorAll('input')
 
     inputs.forEach(input => input.setAttribute("autocomplete","off"));
-    
-    
 
-    
-    
     // .wrapper__title innerHTML
     let placesCardsTitles = document.querySelectorAll(".places-card__title"),
     wraperTitles = document.querySelectorAll(".wrapper__title"),
@@ -14,14 +10,21 @@
     for(i = 0; i < placesCardsTitles.length; i++) {
       wraperTitles[i].innerHTML = placesCardsTitles[i].innerHTML
     }
-    
+
+
+  // autocomplite script
+
+    let el = document.getElementById('city');
+    el.addEventListener('click', function() {
+      
+    })
+
 
 
 
     // import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 
 
-   
       const swiper = new Swiper('.swiper-container', {
         cssMode: true,
         navigation: {
@@ -34,4 +37,13 @@
         mousewheel: true,
         keyboard: true,
       });
-   
+
+
+      // view-All-Offers
+
+      // let viewAllOffers = document.querySelector(".more-cards")
+      // let viewAllOffersButton = document.querySelector(".more-cards__button")
+
+      // viewAllOffersButton.addEventListener('click', function() {
+
+      // })
