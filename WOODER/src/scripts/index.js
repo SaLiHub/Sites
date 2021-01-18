5b0f1fed174ed1c7593aebe1cc2c94fae564a0e3
SaLi <sashalischinskiy72@gmail.com>
2021-01-18 15:46:53 +0200

    0.04

--- WOODER/src/scripts/index.js
+++ WOODER/src/scripts/index.js
@@ -3,14 +3,34 @@
 let hamburger = document.querySelector('.header__hamburger')
 let hamburgerMenuText = document.querySelector('.header__hamburger-text')
 let hamburgerMenu = document.querySelector('.header__hamburger-menu')
-
-
+let headerCloseSign = document.querySelector('.header__toggle-menu-cross-sign')
+let pageBody = document.querySelector('.page__body')
 hamburger.addEventListener('click', ()=> {
-    hamburgerMenu.classList.add('active')
+    pageBody.classList.add('opacity_off')
     hamburgerMenu.style.cursor = "auto"
+    setTimeout(() => {
+        hamburgerMenu.classList.add('active') 
+        
+        pageBody.classList.remove('opacity_off')
+        }, 500);
+        
 })
 
 hamburgerMenuText.addEventListener('click', ()=> {
-    hamburgerMenu.classList.add('active')
+    pageBody.classList.add('opacity_off')
     hamburgerMenu.style.cursor = "auto"
+    setTimeout(() => {
+        hamburgerMenu.classList.add('active') 
+        
+        pageBody.classList.remove('opacity_off')
+        }, 500);
+})
+
+headerCloseSign.addEventListener('click', () => {
+    pageBody.classList.add('opacity_off')
+    setTimeout(() => {
+        hamburgerMenu.classList.remove('active')
+        pageBody.classList.remove('opacity_off')
+        }, 500);
+    hamburgerMenu.style.cursor = "pointer"
 })
