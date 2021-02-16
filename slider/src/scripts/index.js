25beb5e63302e7071f74b65a2d194b56484c2b04
SaLi <sashalischinskiy72@gmail.com>
2021-02-16 23:26:36 +0200

    0.12

--- slider/src/scripts/index.js
+++ slider/src/scripts/index.js
@@ -15,10 +15,12 @@
     y,
     startX,
     x,
-    slideHeight = slides[0].offsetHeight,
-    slideWidth = slides[0].offsetWidth;
+    slideHeight = sliderContainer.offsetHeight,
+    slideWidth = sliderContainer.offsetWidth,
+    decrement = 0.2;
     
-
+    
+    
     // adding transparent highlight
    
         sliderContainer.classList.add('highlight-transparent');
@@ -53,10 +55,10 @@ for (let i = 0; i < slides.length; i++) {
             
             if(sliderDirection === "horizontal") {
                 slidePosition = slideWidth*i;
-                sliderWrapper.style.transform = `translate3d(-${slidePosition}px, 0, 0)`;
+                sliderWrapper.style.transform = `translate3d(${-slidePosition}px, 0, 0)`;
             } else if (sliderDirection === "vertical") {
                 slidePosition = slideHeight*i;
-                sliderWrapper.style.transform = `translate3d(0, -${slidePosition}px, 0)`;
+                sliderWrapper.style.transform = `translate3d(0, ${-slidePosition}px, 0)`;
             }
             sliderWrapper.style.transitionDuration  = '300ms';
             document.querySelector('.active-slide').classList.remove('active-slide');
@@ -77,10 +79,10 @@ for (let i = 0; i < slides.length; i++) {
     slides[numberOfSlide].classList.add('active-slide');
     if(sliderDirection === "horizontal") {
         slidePosition = slideWidth*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(-${slidePosition}px, 0, 0)`;
+        sliderWrapper.style.transform = `translate3d(${-slidePosition}px, 0, 0)`;
     } else if (sliderDirection === "vertical") {
         slidePosition = slideHeight*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(0, -${slidePosition}px, 0)`;
+        sliderWrapper.style.transform = `translate3d(0, ${-slidePosition}px, 0)`;
     }
     
  })();
@@ -89,16 +91,18 @@ for (let i = 0; i < slides.length; i++) {
 
 
  const start = (e) => {
-     
+    sliderWrapper.style.transitionDuration  = '0ms';
+   
     if(e.buttons === 2) return;
      pressed = true;
+    
      if(sliderDirection === "horizontal") { 
          startX = e.pageX;
      } else if (sliderDirection === "vertical") {
          startY = e.pageY;
      }
      
-     sliderContainer.style.cursor = 'grab'
+    
     
  }
 
@@ -106,69 +110,102 @@ for (let i = 0; i < slides.length; i++) {
  const move = (e) => {
     if(!pressed) return;
     e.preventDefault();
-
+   
     if(sliderDirection === "horizontal") {
         const dist = e.pageX - startX;
-        if(dist > 0 && slides[0].classList.contains('active-slide')) return;
-        if(dist < 0 && slides[slides.length - 1].classList.contains('active-slide')) return;
-        x = dist;
-      
-        sliderWrapper.style.transform = `translate3d(-${slidePosition - dist}px, 0, 0)`
+        if(dist > 0 && slides[0].classList.contains('active-slide')) {
+            decrement += 0.576;
+            // console.log(decrement)
+            sliderWrapper.style.transform = `translate3d(${-(slidePosition - (dist - decrement))}px, 0, 0)`
+        } else {
+            x = dist;
+        
+            sliderWrapper.style.transform = `translate3d(${-(slidePosition - dist)}px, 0, 0)`
+        }
+        // if(dist < 0 && slides[slides.length - 1].classList.contains('active-slide')) return;
+       
+       
     } else if (sliderDirection === "vertical") {
         const dist = e.pageY - startY;
-        if(dist > 0 && slides[0].classList.contains('active-slide')) return;
-        if(dist < 0 && slides[slides.length - 1].classList.contains('active-slide')) return;
+   
+        if(dist < 0 && slides[slides.length - 1].classList.contains('active-slide')) {
+            decrement += 0.576;
+            console.log(decrement)
+            sliderWrapper.style.transform = `translate3d(0,${-(slidePosition - (dist - decrement))}px, 0)`
+        }else {
+            y = dist;
+            sliderWrapper.style.transform = `translate3d(0, ${-(slidePosition - dist)}px, 0)`
+        };
         
         y = dist;
-        sliderWrapper.style.transform = `translate3d(0, -${slidePosition - dist}px, 0)`
+        sliderWrapper.style.transform = `translate3d(0, ${-(slidePosition - dist)}px, 0)`
     }
 }
 
  const end = (e) => {
+     if(!pressed) return;
    
+
+    if(numberOfSlide + 1 === slides.length || numberOfSlide === 0) {
+        
+        currentSlide()
+    }
+    
     if(sliderDirection === "horizontal") {
-        if(pressed && x < 0) {
-            
-            nextSlider()
-        } else if((pressed && x > 0)){
-            
+        if(pressed && x > 0 && !slides[0].classList.contains('active-slide')){
+    
             prevSlider() 
+        } else if(pressed && x < 0 && !slides[slides.length - 1].classList.contains('active-slide')) {
+            nextSlider()
         }
     } else if (sliderDirection === "vertical") {
-        if(pressed && y < 0) {
-            nextSlider()
-        } else if((pressed && y > 0)){
+        if(pressed && y > 0 && !slides[0].classList.contains('active-slide')) {
             prevSlider() 
+        } else if((pressed && y < 0 && !slides[slides.length - 1].classList.contains('active-slide'))){
+            nextSlider()
         }
     }
+    
     pressed = false;
-    sliderContainer.style.cursor = 'pointer';
+    decrement = 0.2;
 }
 
 
 window.addEventListener('resize', () => {
-    slideHeight = slides[0].offsetHeight;
+    
     if(sliderDirection === "horizontal") {
         slideWidth = slides[0].offsetWidth;
         slidePosition = slideWidth*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(-${slidePosition}px, 0, 0)`
+        sliderWrapper.style.transform = `translate3d(${-slidePosition}px, 0, 0)`
     } else {
         slideHeight = slides[0].offsetHeight;
         slidePosition = slideHeight*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(0, -${slidePosition}px, 0)`
+        sliderWrapper.style.transform = `translate3d(0, ${-slidePosition}px, 0)`
     }
 })
 
+const currentSlide = () => {
+    if(sliderDirection === "horizontal") {
+        slidePosition = slideWidth*numberOfSlide;
+        sliderWrapper.style.transform = `translate3d(${-slidePosition}px, 0, 0)`;
+        sliderWrapper.style.transitionDuration  = '300ms';
+    } else {
+        slidePosition = slideHeight*numberOfSlide;
+        sliderWrapper.style.transitionDuration  = '300ms';
+        sliderWrapper.style.transform = `translate3d(0, ${-slidePosition}px, 0)`
+    }
+    
+}
 
 const prevSlider = () => {
-    if(slides[0].classList.contains('active-slide')) return;
+    
     numberOfSlide--
     if(sliderDirection === "horizontal") {
         slidePosition = slideWidth*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(-${slidePosition}px, 0, 0)`;
+        sliderWrapper.style.transform = `translate3d(${-slidePosition}px, 0, 0)`;
     } else if (sliderDirection === "vertical") {
         slidePosition = slideHeight*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(0, -${slidePosition}px, 0)`;
+        sliderWrapper.style.transform = `translate3d(0, ${-slidePosition}px, 0)`;
     }
     slides[numberOfSlide+1].classList.remove('active-slide');
     slides[numberOfSlide].classList.add('active-slide');
@@ -176,6 +213,7 @@ const prevSlider = () => {
     bullets[numberOfSlide].classList.add('active-bullet');
     sliderWrapper.style.transitionDuration  = '300ms';
 
+    // counter
     if(numberOfSlide+1 >= 10) {
         counter.innerHTML = `${numberOfSlide+1}`
     } else {
@@ -184,15 +222,14 @@ const prevSlider = () => {
 }
 
 const nextSlider = () => {
-    if(slides[slides.length - 1].classList.contains('active-slide')) return;
     
     numberOfSlide++
     if(sliderDirection === "horizontal") {
         slidePosition = slideWidth*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(-${slidePosition}px, 0, 0)`;
+        sliderWrapper.style.transform = `translate3d(${-slidePosition}px, 0, 0)`;
     } else if (sliderDirection === "vertical") {
         slidePosition = slideHeight*numberOfSlide;
-        sliderWrapper.style.transform = `translate3d(0, -${slidePosition}px, 0)`;
+        sliderWrapper.style.transform = `translate3d(0, ${-slidePosition}px, 0)`;
     }
     slides[numberOfSlide-1].classList.remove('active-slide');
     slides[numberOfSlide].classList.add('active-slide');
@@ -200,6 +237,7 @@ const nextSlider = () => {
     bullets[numberOfSlide].classList.add('active-bullet');
     sliderWrapper.style.transitionDuration  = '300ms';
 
+    // counter
     if(numberOfSlide+1 >= 10) {
         counter.innerHTML = `${numberOfSlide+1}`
     } else {
@@ -214,22 +252,31 @@ sliderWrapper.addEventListener('transitionend', () => {
 });
 
 
-    
-    sliderContainer.addEventListener('pointerdown', start);
-    sliderContainer.addEventListener('mousedown', start);
-    // sliderContainer.addEventListener('touchstart', start);
-    
-    sliderContainer.addEventListener('pointermove', move);
-    sliderContainer.addEventListener('mousemove', move);
-    // sliderContainer.addEventListener('touchmove', move);
-   
-    sliderContainer.addEventListener('pointerup', end);
-    sliderContainer.addEventListener('mouseup', end);
-    // sliderContainer.addEventListener('touchcancel', end);
+    if(window.PointerEvent) {
+        console.log('pointer')
+        sliderContainer.addEventListener('pointerdown', start);
+        sliderContainer.addEventListener('pointermove', move);
+        sliderContainer.addEventListener('pointerup', end);
+        sliderContainer.addEventListener('pointercancel', end);
 
-    sliderContainer.addEventListener('mouseleave', end);
-    sliderContainer.addEventListener('pointercancel', end);
-    // sliderContainer.addEventListener('touchend', end);
+        // 
+        
+        sliderContainer.addEventListener('mouseleave', end);
+        
+    } else {
+        console.log('mouse')
+        sliderContainer.addEventListener('mousedown', start);
+        sliderContainer.addEventListener('mousemove', move);
+        sliderContainer.addEventListener('mouseup', end);
+        sliderContainer.addEventListener('mouseleave', end);
+
+        sliderContainer.addEventListener('touchstart', start);
+        sliderContainer.addEventListener('touchmove', move);
+        sliderContainer.addEventListener('touchcancel', end);
+        sliderContainer.addEventListener('touchend', end);
+    }
+
+  
    
     
 
