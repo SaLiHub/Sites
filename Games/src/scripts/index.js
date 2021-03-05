bffe3838bc10e06f0457f6b0935f1d8ca472c273
SaLi <sashalischinskiy72@gmail.com>
2021-03-05 13:12:11 +0200

    Add border and hover effects for links
    - add border for footer
    - create gover effects for links in section section-cards-3

--- Games/src/scripts/index.js
+++ Games/src/scripts/index.js
@@ -1,45 +1,42 @@
 (() => {
   // slider1
-  const swiper1 = new Swiper('#swiper-banner', {
-    direction: 'vertical',
+  const swiper1 = new Swiper("#swiper-banner", {
+    direction: "vertical",
     slidesPerView: 1,
     slidesPerGroup: 1,
-    effect: 'cube',
+    effect: "cube",
     grabCursor: true,
     cubeEffect: {
       shadow: false,
       slideShadows: false,
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
   });
 
   //slider2
-  const swiper2 = new Swiper('#swiper-container-1', {
+  const swiper2 = new Swiper("#swiper-container-1", {
     slidesPerView: 4,
     spaceBetween: 10,
     slidesPerGroup: 1,
     navigation: {
-      prevEl: '#swiper-button-prev-1',
-      nextEl: '#swiper-button-next-1',
+      prevEl: "#swiper-button-prev-1",
+      nextEl: "#swiper-button-next-1",
     },
   });
 
   //section 3
-  const swiper3 = new Swiper('#swiper-container-2', {
-    slidesPerView: 'auto',
+  const swiper3 = new Swiper("#swiper-container-2", {
+    slidesPerView: "auto",
     centeredSlides: true,
     spaceBetween: 130,
     loop: true,
     // touchRatio: 0,
     navigation: {
-      nextEl: '#swiper-button-next-2',
-      prevEl: '#swiper-button-prev-2',
+      nextEl: "#swiper-button-next-2",
+      prevEl: "#swiper-button-prev-2",
     },
   });
-
-
-
 })();
