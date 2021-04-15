c81df35f7c8ce3d17c43b2b04ed5a0fab83bc9f7
SaLi <sashalischinskiy72@gmail.com>
2021-04-15 13:12:32 +0300

    Change gulp file and refactor code

--- Games/src/scripts/index.js
+++ Games/src/scripts/index.js
@@ -1,42 +1,44 @@
-(() => {
+'use strict';
+
+(function executeAllSliders() {
   // slider1
-  const swiper1 = new Swiper("#swiper-banner", {
-    direction: "vertical",
+  Swiper('#swiper-banner', {
+    direction: 'vertical',
     slidesPerView: 1,
     slidesPerGroup: 1,
-    effect: "cube",
+    effect: 'cube',
     grabCursor: true,
     cubeEffect: {
       shadow: false,
       slideShadows: false,
     },
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
       clickable: true,
     },
   });
 
-  //slider2
-  const swiper2 = new Swiper("#swiper-container-1", {
+  // slider2
+  Swiper('#swiper-container-1', {
     slidesPerView: 4,
     spaceBetween: 10,
     slidesPerGroup: 1,
     navigation: {
-      prevEl: "#swiper-button-prev-1",
-      nextEl: "#swiper-button-next-1",
+      prevEl: '#swiper-button-prev-1',
+      nextEl: '#swiper-button-next-1',
     },
   });
 
-  //section 3
-  const swiper3 = new Swiper("#swiper-container-2", {
-    slidesPerView: "auto",
+  // section 3
+  Swiper('#swiper-container-2', {
+    slidesPerView: 'auto',
     centeredSlides: true,
     spaceBetween: 130,
     loop: true,
     // touchRatio: 0,
     navigation: {
-      nextEl: "#swiper-button-next-2",
-      prevEl: "#swiper-button-prev-2",
+      nextEl: '#swiper-button-next-2',
+      prevEl: '#swiper-button-prev-2',
     },
   });
 })();
