3fdd42a127c2967c3033640f3b2940f5c3e34d60
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-07 14:01:04 +0300

    Refactor code

--- Games/gulpfile.js
+++ Games/gulpfile.js
@@ -12,73 +12,73 @@ import csso from 'postcss-csso';
 // js
 import eslint from 'gulp-eslint';
 import terser from 'gulp-terser';
-import prettier from 'gulp-prettier';
 
 // html
 import htmlmin from 'gulp-htmlmin';
 
-// dirrections
-const TEST = 'test',
-  DIST = 'dist',
-  SRC = 'src';
+// Set directories.
+const TEST = 'test';
+const DIST = 'dist';
+const SRC = 'src';
 
-// copy html files from SRC to TEST
-const html = () => {
-  return gulp
+// Copy html files from SRC to TEST.
+const html = () =>
+  gulp
     .src(`${SRC}/*.html`)
     .pipe(gulp.dest(`${TEST}`))
     .pipe(sync.stream());
-};
+// Copy styles from SRC to TEST with few modifications.
+const styles = () =>
+  gulp
+    .src(`${SRC}/styles/index.css`)
+    // 'pimport' resolve path of an @import rule
+    // 'autoprefixer' add or remove vendor prefixes like -webkit and -moz after checking the code
+    .pipe(postcss([pimport, autoprefixer]))
+    .pipe(gulp.dest(`${TEST}/styles`))
+    .pipe(sync.stream());
 
-// copy styles from SRC to TEST with few modifications
-const styles = () => {
-  return (
-    gulp
-      .src(`${SRC}/styles/index.css`)
-      // 'pimport' resolve path of an @import rule
-      // 'autoprefixer' add or remove vendor prefixes like -webkit and -moz after checking the code
-      .pipe(postcss([pimport, autoprefixer]))
-      .pipe(gulp.dest(`${TEST}/styles`))
-      .pipe(sync.stream())
-  );
-};
-
-const imagesMain = (dir) => {
-  return function images() {
-    return gulp.src(`${SRC}/images/**/*`).pipe(gulp.dest(`${dir}/images`));
+const imagesMain = (dir) =>
+  function images() {
+    return gulp
+      .src(`${SRC}/images/**/*`)
+      .pipe(gulp.dest(`${dir}/images`))
+      .pipe(sync.stream());
   };
-};
 
-const fontsMain = (dir) => {
-  return function fonts() {
-    return gulp.src(`${SRC}/fonts/**/*`).pipe(gulp.dest(`${dir}/fonts`));
+const fontsMain = (dir) =>
+  function fonts() {
+    return gulp
+      .src(`${SRC}/fonts/**/*`)
+      .pipe(gulp.dest(`${dir}/fonts`))
+      .pipe(sync.stream());
   };
-};
 
-// copy scripts and modify with bubel
-const scripts = () => {
-  return gulp
+// Copy scripts and modify with babel.
+const scripts = () =>
+  gulp
     .src(`${SRC}/scripts/index.js`)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
     .pipe(babel())
-    .pipe(prettier())
-    .pipe(gulp.dest(`${TEST}/scripts`));
-};
+    .pipe(gulp.dest(`${TEST}/scripts`))
+    .pipe(sync.stream());
 
 // browser sync
 const server = () => {
   sync.init({
     // more info https://browsersync.io/docs/options#option-ui
     ui: false,
-    // shows any notifications in the browser if true
+    // Shows any notifications in the browser if true.
     notify: false,
-    // dirrectory wich will be displayed in browser
+    // Set directory which will be displayed in browser.
     server: {
       baseDir: TEST,
     },
   });
 };
 
-// change "test" files according to src's when any changes occur
+// Change "test" files according to src when any changes occur.
 const watch = () => {
   gulp.watch(`${SRC}/*.html`, html);
   gulp.watch(`${SRC}/styles/**/*.css`, styles);
@@ -87,28 +87,23 @@ const watch = () => {
   gulp.watch(`${SRC}/images/**/*`, imagesMain(TEST));
 };
 
-// executes when you run "gulp build" in console
-export const build = () => {
-  return new Promise(function (res, rej) {
+// Executes when you run "gulp build" in console.
+export const build = () =>
+  new Promise((res) => {
     buildMain();
     res();
   });
-};
 
-// build function
 function buildMain() {
-  // check with eslint by config rules in ".eslintrc.json"
-  linterMain(TEST);
-  // minify js, css, html
+  // Minify js, css, html.
   htmlMinify();
   cssMinify();
   jsMinify();
-  // transfer images and fonts into folder in variable DIST
+  // Transfer images and fonts into folder in variable DIST.
   imagesMain(DIST)();
   fontsMain(DIST)();
 }
 
-// minifying funtions
 function htmlMinify() {
   gulp
     .src(`${TEST}/*.html`)
@@ -130,20 +125,19 @@ function jsMinify() {
   gulp
     .src(`${TEST}/scripts/index.js`)
     .pipe(terser())
-    .pipe(gulp.dest(`${DIST}/js`));
+    .pipe(gulp.dest(`${DIST}/scripts`));
 }
 
-export const linter = () => {
-  return new Promise(function (res, rej) {
+export const linter = () =>
+  new Promise((res) => {
     linterMain();
     res();
   });
-};
 
-// linter function
 function linterMain(dir) {
-  if (!dir) dir = SRC;
-  gulp.src(`${dir}/scripts/index.js`).pipe(eslint()).pipe(eslint.format());
+  let newDir;
+  if (!dir) newDir = SRC;
+  gulp.src(`${newDir}/scripts/index.js`).pipe(eslint()).pipe(eslint.format());
 }
 
 export default gulp.series(
