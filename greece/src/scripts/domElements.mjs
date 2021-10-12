7b3f8dba524933940ec7014fb7eabe361b976d1d
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-12 19:01:44 +0300

    Add domElements module

---
+++ greece/src/scripts/domElements.mjs
@@ -0,0 +1,23 @@
+function getDomElements() {
+  return {
+    pageBody: document.querySelector('.page__body'),
+    inputs: document.querySelectorAll('input'),
+    placesCardTitles: document.querySelectorAll('.places-card__title'),
+    wrapperTitles: document.querySelectorAll('.places-card-wrapper__title'),
+    viewAllOffers: document.querySelector('.more-cards__button'),
+    cardsContainer: document.querySelector('.cards'),
+    smallFormButton: document.querySelector('.search__show-small-form'),
+    smallScreenFormWrapper: document.querySelector('.search__small-form-wrapper'),
+    smallFormCrossSign: document.querySelector('.search__small-form-cross-sign'),
+    submit: document.querySelector('.submit'),
+    placesCardButtons: document.querySelectorAll('.places-card-buttons'),
+    placesCard: document.querySelectorAll('.places-card'),
+    smallMenu: document.querySelector('.small-menu'),
+    smallMenuLinks: document.querySelectorAll('.small-menu__link'),
+    ham: document.querySelector('.ham'),
+    formInputs: document.querySelectorAll('.form__input'),
+    calendars: document.querySelectorAll('.search__calendar'),
+  };
+}
+
+export default getDomElements();
