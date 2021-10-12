a77c9b307ec480832c3e571d9594f36b0ee5a1d1
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-12 19:03:24 +0300

    Create connectDB module

---
+++ greece/src/scripts/connectDB.mjs
@@ -0,0 +1,24 @@
+function connectDB() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyA_BlS_0vucMidOD7OzEWND9kwPw7ojg_c',
+    authDomain: 'greece-d2a3d.firebaseapp.com',
+    databaseURL: 'https://greece-d2a3d-default-rtdb.firebaseio.com',
+    projectId: 'greece-d2a3d',
+    storageBucket: 'greece-d2a3d.appspot.com',
+    messagingSenderId: '769401518786',
+    appId: '1:769401518786:web:eec291fd613c097c59d95b',
+    measurementId: 'G-C8E617QPC4',
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  // Get a reference to the database service.
+  function databaseInit() {
+    return firebase.database().ref('/').once('value');
+  }
+
+  return databaseInit()
+    .then((answer) => answer.val());
+}
+
+export default connectDB;
