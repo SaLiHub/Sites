b475745aa2ede5eb8f001d74f48c4093f15c2143
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-13 20:32:17 +0300

    Add elements

--- greece/src/scripts/domElements.mjs
+++ greece/src/scripts/domElements.mjs
@@ -3,7 +3,8 @@ function getDomElements() {
     pageBody: document.querySelector('.page__body'),
     inputs: document.querySelectorAll('input'),
     placesCardTitles: document.querySelectorAll('.places-card__title'),
-    wrapperTitles: document.querySelectorAll('.places-card-wrapper__title'),
+    wrapperTitles: document.querySelectorAll('.places-card__wrapper-title'),
+    wrappers: document.querySelectorAll('.places-card__wrapper'),
     viewAllOffers: document.querySelector('.more-cards__button'),
     cardsContainer: document.querySelector('.cards'),
     smallFormButton: document.querySelector('.search__show-small-form'),
@@ -11,12 +12,14 @@ function getDomElements() {
     smallFormCrossSign: document.querySelector('.search__small-form-cross-sign'),
     submit: document.querySelector('.submit'),
     placesCardButtons: document.querySelectorAll('.places-card-buttons'),
-    placesCard: document.querySelectorAll('.places-card'),
+    buttons: document.querySelectorAll('.places-card-buttons__button'),
+    placesCards: document.querySelectorAll('.places-card'),
     smallMenu: document.querySelector('.small-menu'),
     smallMenuLinks: document.querySelectorAll('.small-menu__link'),
     ham: document.querySelector('.ham'),
     formInputs: document.querySelectorAll('.form__input'),
     calendars: document.querySelectorAll('.search__calendar'),
+
   };
 }
 
