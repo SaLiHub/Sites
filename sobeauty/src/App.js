419edd7435668d6bef42a022bce9cd736810bae4
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-11-21 11:12:22 +0200

    Add main components

---
+++ sobeauty/src/App.js
@@ -0,0 +1,14 @@
+import './App.sass';
+import Header from './components/header/Header';
+import Banner from './components/banner/Banner'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
