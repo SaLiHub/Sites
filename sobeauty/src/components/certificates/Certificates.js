4195168a4bef2b9af8fb8007f3cad8e58f4bb7f1
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-01-04 14:54:08 +0200

    Add accessibility to slider

--- sobeauty/src/components/certificates/Certificates.js
+++ sobeauty/src/components/certificates/Certificates.js
@@ -9,21 +9,22 @@ import 'swiper/modules/navigation/navigation.min.css';
 import './Certificates.sass';
 
 // Import Swiper core and required modules.
-import SwiperCore, { Navigation } from 'swiper';
+import SwiperCore, { Navigation, A11y  } from 'swiper';
 
 // Install Swiper modules.
-SwiperCore.use([Navigation]);
+SwiperCore.use([Navigation, A11y]);
 
 function Certificates() {
   return (
-    <section className='certificates'>
+    <section className='certificates app__certificates'>
       <div className='container'>
         <h2 className='certificates__title'>Сертифікати</h2>
         <Swiper
           slidesPerView={6}
           spaceBetween={30}
-          navigation={true}
-          className="mySwiper"
+          navigation
+          a11y
+          className="mySwiper"  
         >
           <SwiperSlide className='certificates__card'>Card</SwiperSlide>
           <SwiperSlide className='certificates__card'>Card</SwiperSlide>
