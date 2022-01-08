0d73943fa0f696238392b955b6da2f4ebc41970d
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-01-08 15:54:42 +0200

    Create new form

--- sobeauty/src/components/order/Order.js
+++ sobeauty/src/components/order/Order.js
@@ -1,36 +1,53 @@
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
 
-import './Order.sass'
+import './Order.scss';
 
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
+// import "mdbreact/dist/css/mdb.css";
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 function Order() {
-  return(
+  return (
     <section className="order">
       <div className="container">
-      <Form className="order__form">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+        <div className="order__wrapper">
+          <FormPage/>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
 
+function FormPage (){
+  return (
+  <MDBContainer>
+    <MDBRow>
+      <MDBCol md="6" className="order__title-wrapper">
+        <p className="h5 text-center mb-4">Замовте послугу прямо зараз</p>
+      </MDBCol>
+  
+      <MDBCol md="6">
+        <form>
+          <div className="grey-text">
+            <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
+              success="right" />
+            <MDBInput label="Your telephone" icon="phone" minLength="9" maxLength="14" group type="tel" pattern="^\+?3?8?(0\d{9})$" validate error="wrong"
+              success="right" />
+          </div>
+          <div className="text-center">
+            <MDBBtn outline color="info">
+              Send
+              <MDBIcon far icon="paper-plane" className="ml-1" />
+            </MDBBtn>
+          </div>
+        </form>
+      </MDBCol>
+    </MDBRow>
+  </MDBContainer>
+  );
+  };
+  
+
 export default Order;
+
